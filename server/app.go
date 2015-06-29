b8e7894719abe553e267802433a6b8adfe26cdd9
Geoffrey Bauduin <bauduin.geo@gmail.com>
2015-06-29 10:30:36 +0200

    wip

---
+++ server/app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	
+	router := mux.NewRouter().StrictSlash(true)
+	
+	router.HandleFunc("/", Index)
+	
+	log.Fatal(http.ListenAndServe(":4444", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	 fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
